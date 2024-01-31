50101995f10e57212f86210de776453da9949659
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-30 22:16:12 -0300

    Refactor TwoSum function

--- Two Sum/exercise.js
+++ Two Sum/exercise.js
@@ -26,13 +26,13 @@ Output: [0,1]
 
 */
 
-var TwoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length ; j++) {
-            const sum = nums[i] + nums[i + j]
-            if (sum === target) {
-                return [i, i + j]
-            }
-        }
+var TwoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      const sum = nums[i] + nums[i + j];
+      if (sum === target) {
+        return [i, i + j];
+      }
     }
+  }
 };
